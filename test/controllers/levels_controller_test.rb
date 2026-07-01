1a5253ad44bcc3ab5e639bb9555659a2cb325057
Ruby Puzzle Lab User <ruby@puzzle-lab.local>
2026-07-01 18:58:35 +0200

    Update user email in levels_controller_test for consistency

--- test/controllers/levels_controller_test.rb
+++ test/controllers/levels_controller_test.rb
@@ -4,14 +4,14 @@ class LevelsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   test "should get index" do
-    user = User.create!(email: "index@example.com", password: "password123", password_confirmation: "password123")
+    user = User.create!(email: "s9457558@outlook.it", password: "d9KVUv9!WGA0ZpgK", password_confirmation: "d9KVUv9!WGA0ZpgK")
     sign_in user
     get levels_path
     assert_response :success
   end
 
   test "should show an answer input field on the level page" do
-    user = User.create!(email: "test@example.com", password: "password123", password_confirmation: "password123")
+    user = User.create!(email: "s9457558@outlook.it", password: "d9KVUv9!WGA0ZpgK", password_confirmation: "d9KVUv9!WGA0ZpgK")
     level = Level.create!(title: "Livello prova", description: "Prova", level_type: "drag-and-drop", solution: "puts 'ciao'")
     level.blocks.create!(content: "puts")
     level.blocks.create!(content: "'ciao'")
